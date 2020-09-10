6ed0c638f53bc80f5173c844723ddd575e978c75
NaJ <naj10000@live.com>
2020-09-10 14:47:33 -0400

    uselocalstorage

--- src/index.js
+++ src/index.js
@@ -18,8 +18,9 @@ const App = () => {
       .then(res => setCoinData(res.data))
       .catch(err => console.log(err));
   }, []);
+  // {darkmode ? "dark-mode App" : "App"}
   return (
-    <div className={darkmode ? "dark-mode App" : "App"}>
+    <div className="App">
       <Navbar />
       <Charts coinData={coinData} />
     </div>
